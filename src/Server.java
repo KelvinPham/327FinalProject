84d453441cc956e24deb60e252c5224fb29bf7a4
Ryan Ea <helloxhi1@gmail.com>
2017-05-10 06:47:42 -0700

    forgot to upload server file

--- src/Server.java
+++ src/Server.java
@@ -1,142 +1,143 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
 import java.util.concurrent.locks.*;
 
 public class Server {
-	public static final int PORT = 1337;
+	static int client = 0;
 	public static Lock fibLock = new ReentrantLock();
 	public static Lock primeLock = new ReentrantLock();
 	public static Lock randLock = new ReentrantLock();
-	private static int option = 0;
-	public static int nextEvenFib(int n) {
-		if (n == 1) {
-			return 2;
-		} else if (n < 1) {
-			return n;
-		}
-		int fibonacci = ((4 * nextEvenFib(n - 1)) + nextEvenFib(n - 2));
-		return fibonacci;
-
-	}
-
-	public static int nextLargerRand(int prevNumber, int max) {
-		Random random = new Random();
-		int rand;
-		do {
-			rand = random.nextInt();
-		} while (rand < prevNumber);
-
-		return rand;
-	}
-
-	public static int nextPrime(int n) {
-		boolean isPrime = false;
-		while (!isPrime) {
-			n += 1;
-			isPrime = true;
-			if (n <= 2) {
-				return 2;
+	public static void main(String[] args) throws Exception {
+		System.out.println("Server Started");
+		HashMap<String, Integer> a = new HashMap<String,Integer>();
+		a.put("evenFib", 1);
+		a.put("nextPrime",1);
+		a.put("nextRand", 1);
+		ServerSocket server = new ServerSocket(1337);//Creates server at port 1337
+		try{
+			while(true){
+				new ClientConnection(server.accept() , ++client, a).start();//Creates a thread for each client
 			}
-			for (int i = 2; i <= Math.ceil(Math.sqrt(n)); i++) {
-				if (n % i == 0) {
-					isPrime = false;
-					break;
+		}
+		finally {
+			server.close();
+		}
+	}
+	private static class ClientConnection extends Thread {
+		private Socket socket;
+		private int ClientNumber;
+		HashMap<String, Integer> numberSet;
+		private static ArrayList<Integer> primeList= new ArrayList<Integer>();
+		Random rand = new Random();
+		public ClientConnection(Socket connection, int client, HashMap<String, Integer> values){
+			this.socket=connection;
+			this.ClientNumber=client;
+			this.numberSet= values;
+			System.out.println("Client #"+ ClientNumber +  " connected to the server");
+		}
+
+
+
+		public void run(){
+			try{
+				BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+				while(true){
+					String input = in.readLine();
+					int a= Integer.parseInt(input);
+					switch(a){
+						case 2://Returns a fibbonaci number
+							fibLock.lock();
+							if(numberSet.get("evenFib") == 16)
+								numberSet.put("evenFib", 1);
+							int fibonacci = nextEvenFib(numberSet.get("evenFib"));
+							numberSet.put("evenFib", numberSet.get("evenFib")+1);
+							System.out.println("Sending Fibonacci: "
+									+ fibonacci);
+							out.println(fibonacci);
+							fibLock.unlock();
+							break;
+						case 3://Returns a random that is +1-10 value higher
+							randLock.lock();
+							int value =nextLargerRand(numberSet.get("nextRand"));
+							out.println(value);
+							numberSet.put("nextRand", value);
+							randLock.unlock();
+							break;
+						case 4://Returns the next bigger prime number
+							primeLock.lock();
+							int prime = nextPrime(numberSet.get("nextPrime"));
+							if(prime<0) {
+								numberSet.put("nextPrime", 1);
+								prime = nextPrime(numberSet.get("nextPrime"));
+							}
+							numberSet.put("nextPrime", prime);
+							System.out.println("Sending the prime number: "
+									+ prime);
+							out.println(prime);
+							//primeNumber = prime;
+							primeLock.unlock();
+							break;
+						case 5://Closes the thread client
+							out.close();
+							in.close();
+							break;
+						default:
+							out.println("Error");
+					}
+					if(input.equals("quit"))
+						break;
+				}
+			} catch (IOException e) {
+				// e.printStackTrace();
+			}
+			finally {
+				try {
+					System.out.println("Client #" +ClientNumber + " is closed");
+					client--;
+					socket.close();
+				}
+				catch (IOException e){
+					System.out.println("Unable to close socket!");
 				}
 			}
 		}
-		return n;
-	}
-
-	public static void main(String[] args) {
-		ServerSocket serverSocket;
-		HashMap<String, Integer> a = new HashMap<String,Integer>();
-		final HashMap pointer = a;
-		a.put("evenFib", 1);
-		a.put("nextPrime",1);
-		try {
-			serverSocket = new ServerSocket(PORT);
-			int maximum = Integer.MAX_VALUE;
-			while (true) {
-				Thread thread = new Thread(new Runnable() {
-					public void run() {
-						try {
-							Socket socket = serverSocket.accept();
-							// try {
-
-							System.err.println("client connected");
-
-							BufferedReader in = new BufferedReader(
-									new InputStreamReader(
-											socket.getInputStream()));
-
-							PrintWriter out = new PrintWriter(
-									new OutputStreamWriter(
-											socket.getOutputStream()), true);
-
-							int prevNumber = 0;
-							option = -1;
-							System.out.println("??ASD");
-							option = Character.getNumericValue(in.read());
-							switch(option){
-							case 2:
-								fibLock.lock();
-								if(a.get("evenFib") == 16)
-									a.put("evenFib", 1);
-								int fibonacci = nextEvenFib(a.get("evenFib"));
-								a.put("evenFib", a.get("evenFib")+1);
-								System.out.println("Sending Fibonacci: "
-										+ fibonacci);
-								out.println(fibonacci);
-							fibLock.unlock();
-							break;
-							case 3:
-								randLock.lock();
-
-								int newNumber = nextLargerRand(prevNumber,
-										maximum);
-								System.out.println("Sending next large random: "
-										+ newNumber);
-								out.println(newNumber);
-								if (prevNumber >= maximum || prevNumber < 0) {
-									prevNumber = 0;
-								} else {
-									prevNumber = newNumber;
-								}
-							randLock.unlock();
-							break;
-							case 4:
-								primeLock.lock();
-								int prime = nextPrime(a.get("nextPrime"));
-								if(prime<0) {
-									a.put("nextPrime", 1);
-									prime = nextPrime(a.get("nextPrime"));
-								}
-								a.put("nextPrime", prime);
-								System.out.println("Sending the prime number: "
-										+ prime);
-								out.println(prime);
-								//primeNumber = prime;
-							primeLock.unlock();
-
-							break;
-							}
-							out.close();
-							in.close();
-							socket.close();
-						} catch (IOException e) {
-
-							e.printStackTrace();
-						}
+		public int nextLargerRand(int Rand){//Generates a random number that is at least 1 higher than old number
+			Rand+= rand.nextInt(10000)+1;
+			if(Rand> Integer.MAX_VALUE || Rand < 0)
+				Rand=0;//Reset value
+			return Rand;
+		}
+		public static int nextPrime(int n) {
+			boolean isPrime = false;
+			while (!isPrime) {
+				n += 1;
+				isPrime = true;
+				if (n <= 2) {
+					return 2;
+				}
+				for (int i = 2; i <= Math.ceil(Math.sqrt(n)); i++) {
+					if (n % i == 0) {
+						isPrime = false;
+						break;
 					}
-				});
-				thread.run();
-				//thread.start();
+				}
 			}
-		} catch (IOException e) {
-			e.printStackTrace();
+			return n;
+		}
+		public static int nextEvenFib(int n) {
+			if (n == 1) {
+				return 2;
+			} else if (n < 1) {
+				return n;
+			}
+			int fibonacci = ((4 * nextEvenFib(n - 1)) + nextEvenFib(n - 2));
+			return fibonacci;
+
 		}
 	}
 }
